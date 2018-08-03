69526972d0deb02be869bd851fb2194c3b2625cd
oblivion0001 <18366108542@163.com>
2018-08-03 18:07:29 +0800

    retrofit

---
+++ app/src/main/java/top/jplayer/quick_test/ui/activity/RetrofitActivity.java
@@ -0,0 +1,26 @@
+package top.jplayer.quick_test.ui.activity;
+
+import android.widget.FrameLayout;
+
+import top.jplayer.baseprolibrary.ui.activity.CommonToolBarActivity;
+import top.jplayer.quick_test.R;
+
+/**
+ * Created by Obl on 2018/8/3.
+ * top.jplayer.quick_test.ui.activity
+ * call me : jplayer_top@163.com
+ * github : https://github.com/oblivion0001
+ */
+
+public class RetrofitActivity extends CommonToolBarActivity {
+    @Override
+    public int initAddLayout() {
+        return R.layout.activity_retrofit;
+    }
+
+    @Override
+    public void initAddView(FrameLayout rootView) {
+        super.initAddView(rootView);
+
+    }
+}
