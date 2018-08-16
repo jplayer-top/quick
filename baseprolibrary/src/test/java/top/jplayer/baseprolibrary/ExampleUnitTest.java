0e51b12c1956ad311bdc2a35127127ac17d675a4
oblivion0001 <18366108542@163.com>
2018-08-16 14:59:43 +0800

    dimen

--- baseprolibrary/src/test/java/top/jplayer/baseprolibrary/ExampleUnitTest.java
+++ baseprolibrary/src/test/java/top/jplayer/baseprolibrary/ExampleUnitTest.java
@@ -2,7 +2,7 @@ package top.jplayer.baseprolibrary;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.Locale;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -12,7 +12,18 @@ import static org.junit.Assert.*;
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
-        String a = "222(2332)";
-        System.out.println(a.substring(a.indexOf("(")+1,a.lastIndexOf(")")));
+
+        for (int i = 1; i < 200; i++) {
+            String s = String.format(Locale.CHINA, "<dimen name=\"dimen_%ddp\">%ddp</dimen>", i, i);
+            System.out.println(s);
+        }
+    }
+
+    @Test
+    public void getSp() throws Exception {
+        for (int i = 1; i < 40; i++) {
+            String s = String.format(Locale.CHINA, "<dimen name=\"dimen_%dsp\">%dsp</dimen>", i, i);
+            System.out.println(s);
+        }
     }
 }
