76fbda5c12d44694f0024623ab71c5776f021e28
oblivion0001 <18366108542@163.com>
2018-08-29 15:10:58 +0800

    color

--- baseprolibrary/src/main/java/top/jplayer/baseprolibrary/utils/PaletteUtil.java
+++ baseprolibrary/src/main/java/top/jplayer/baseprolibrary/utils/PaletteUtil.java
@@ -2,6 +2,7 @@ package top.jplayer.baseprolibrary.utils;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.support.annotation.DrawableRes;
 import android.support.v7.graphics.Palette;
 import android.view.View;
@@ -30,5 +31,20 @@ public class PaletteUtil {
                 view.setBackgroundColor(vibrant.getRgb());
             }
         });
+    }
+
+    //颜色混合
+    public static int blendColor(int fg, int bg) {
+        int scr = Color.red(fg);
+        int scg = Color.green(fg);
+        int scb = Color.blue(fg);
+        int sa = fg >>> 24;
+        int dcr = Color.red(bg);
+        int dcg = Color.green(bg);
+        int dcb = Color.blue(bg);
+        int color_r = dcr * (0xff - sa) / 0xff + scr * sa / 0xff;
+        int color_g = dcg * (0xff - sa) / 0xff + scg * sa / 0xff;
+        int color_b = dcb * (0xff - sa) / 0xff + scb * sa / 0xff;
+        return ((color_r << 16) + (color_g << 8) + color_b) | (0xff000000);
     }
 }
