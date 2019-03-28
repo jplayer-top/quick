0798b040baaf08f2c61d914ba7dae7704836ef7e
jobl <jobl@wmtcgroup.com>
2019-03-28 09:02:08 +0800

    fix

--- app/src/main/java/top/jplayer/quick_test/ui/fragment/MeFragment.java
+++ app/src/main/java/top/jplayer/quick_test/ui/fragment/MeFragment.java
@@ -1,5 +1,6 @@
 package top.jplayer.quick_test.ui.fragment;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.view.View;
 
@@ -9,12 +10,19 @@ import com.yanzhenjie.permission.AndPermission;
 import com.yanzhenjie.permission.Permission;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+import top.jplayer.baseprolibrary.net.tip.DialogLoading;
 import top.jplayer.baseprolibrary.ui.fragment.SuperBaseFragment;
+import top.jplayer.baseprolibrary.utils.BitmapUtil;
 import top.jplayer.baseprolibrary.utils.CameraUtil;
 import top.jplayer.baseprolibrary.utils.LogUtil;
 import top.jplayer.baseprolibrary.widgets.polygon.PolygonImageView;
@@ -34,6 +42,10 @@ public class MeFragment extends SuperBaseFragment {
     PolygonImageView mIvUserAvatar;
     private Unbinder mUnbinder;
     private File mFile;
+    private DialogLoading mLoading;
+
+    public boolean isZiped = true;
+    private ArrayList<File> mArrayList;
 
     @Override
     public int initLayout() {
@@ -52,19 +64,30 @@ public class MeFragment extends SuperBaseFragment {
         });
     }
 
-
+    @SuppressLint("CheckResult")
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == ImageSelector.IMAGE_REQUEST_CODE && resultCode == RESULT_OK && data != null) {
-            List<String> pathList = data.getStringArrayListExtra(ImageSelectorActivity.EXTRA_RESULT);
-            for (String path : pathList) {
-                mFile = new File(path);
-                String fileName = mFile.getName() + mFile.getPath().substring(mFile.getPath().indexOf("."));
-                LogUtil.e(fileName);
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null) {
+            if (mLoading == null) {
+                mLoading = new DialogLoading(mActivity);
             }
+            mLoading.show();
+            isZiped = false;
+            List<String> pathList = data.getStringArrayListExtra(ImageSelectorActivity.EXTRA_RESULT);
+            Observable.just(pathList).subscribeOn(Schedulers.io()).map(strings -> {
+                Observable.fromIterable(strings).subscribe(s -> mArrayList.add(BitmapUtil.compressImage(new File(s))));
+                return mArrayList;
+            }).observeOn(AndroidSchedulers.mainThread()).subscribe(files -> {
+
+                if (mLoading != null && mLoading.isShowing()) {
+                    mLoading.dismiss();
+                }
+                isZiped = true;
+            });
         }
     }
+
 
     @Override
     public void onDestroy() {
